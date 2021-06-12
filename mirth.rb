e5ac8ca8c8085725cd81eb7ae72fcad18439198b
okonomi <okonomi@oknm.jp>
2021-06-12 19:53:50 +0900

    add endpoints

--- mirth.rb
+++ mirth.rb
@@ -10,12 +10,26 @@ loop do
   puts request_line
 
   method_token, target, version_number = request_line.split
-  response_body = "Received a #{method_token} request to #{target} with #{version_number}"
-  client.puts <<~MSG
-    #{version_number} 200 OK
-    Content-Type: text/plain
+  case [method_token, target]
+  when ["GET", "/show/birthdays"]
+    response_status_code = "200 OK"
+    content_type = "text/plain"
+    response_message = "show birthdays"
+  when ["POST", "/add/birthday"]
+    response_status_code = "201 Created"
+    content_type = "text/plain"
+    response_message = "add birthday"
+  else
+    response_status_code = "200 OK"
+    content_type = "text/plain"
+    response_message = "Received a #{method_token} request to #{target} with #{version_number}"
+  end
 
-    #{response_body}
+  client.puts <<~MSG
+    #{version_number} #{response_status_code}
+    Content-Type: #{content_type}
+
+    #{response_message}
   MSG
   client.close
 end
