9311e5937eb942d3e29677aa54867e1fc7f8c55d
okonomi <okonomi@oknm.jp>
2021-06-12 19:31:07 +0900

    get http request

--- mirth.rb
+++ mirth.rb
@@ -4,12 +4,14 @@ server = TCPServer.new(1337)
 loop do
   client = server.accept
 
-  client.puts "What's your name?"
-  input = client.gets
-  puts "Received #{input.chomp} from a client socket on 1337"
-  client.puts "Hi, #{input.chomp}! You've successfully connected to the server socket."
+  request_line = client.readline
 
-  puts "Closing client socket!"
-  client.puts "Goodbye #{input}"
+  puts "The HTTP request line looks line this:"
+  puts request_line
+
+  method_token, target, version_number = request_line.split
+  response_body = "✅ Received a #{method_token} request to #{target} with #{version_number}"
+
+  client.puts response_body
   client.close
 end
